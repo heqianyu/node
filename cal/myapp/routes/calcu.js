417aa5ac58319c5cf44f8d5a7ada25c2b699aa5b
qianyu <heqianyuszzaj@126.com>
2017-07-30 18:52:25 +0800

    1
    1

--- cal/myapp/routes/calcu.js
+++ cal/myapp/routes/calcu.js
@@ -1,15 +1,15 @@
-// calcu.js
+// calcu.js  我是按照之前select的index值来分配的运算符，其实也可将color与js都放在user里，没有选择是因为原本就考虑实现不同的路由分配。
 var express = require('express');
 var router = express.Router();
 var calcu = require('../models/calcu.js');
 
 //获取计算器的布局文件
-router.get('/', function (req, res, next) {
+router.get('/calu', function (req, res, next) {
     res.render('calcu', {title: '计算器'});
 });
 
 //异步获取计算结果
-router.post('/', function (req, res, next) {
+router.post('/calu', function (req, res, next) {
     var f = req.body.vf, s = req.body.vs, t = req.body.vt;
     var c = new calcu();
     c.plus(s, f, t, function (err, data) {
